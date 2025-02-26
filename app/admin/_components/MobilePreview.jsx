37d995c87132eef019c4c71a4ba3e61a5759a850
Sunnyk16 <kumarsunny1246@gmail.com>
2025-02-26 20:34:26 +0530

    Add placeholder comment in MobilePreview component

--- app/admin/_components/MobilePreview.jsx
+++ app/admin/_components/MobilePreview.jsx
@@ -4,6 +4,7 @@ function MobilePreview() {
   return (
     <div className='p-5 md:fixed'>
       <div className='border-[13px] min-w-[340px] max-w-[400px] border-black h-screen rounded-[40px] m-2 shadow-md shadow-primary '>
+        {/* ...... */}
 
       </div>
     </div>
